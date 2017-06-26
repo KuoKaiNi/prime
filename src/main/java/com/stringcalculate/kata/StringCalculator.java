2b9ee98a5aa632a384dbdd0585fed41935754003
Guo KaiNi <ken@webcomm.com.tw>
2017-06-26 23:51:51 +0800

    testSupportDifferentDelimiters Refactor

--- src/main/java/com/stringcalculate/kata/StringCalculator.java
+++ src/main/java/com/stringcalculate/kata/StringCalculator.java
@@ -2,35 +2,40 @@ package com.stringcalculate.kata;
 
 public class StringCalculator {
 
-	public int add(String numbers) throws RuntimeException {
-		int result = 0;
-		
+	private String split = ",";
+	
+	
+	private String parserEmptyString(String numbers){
 		if (numbers.isEmpty()){
-			return result;
+			numbers = "0";
 		}
-		
-		String split = ",";
-		
+		return numbers;
+	}
+	
+	private String setupDefaultSplit(String numbers){
 		if (numbers.indexOf("//") == 0){
 			split = numbers.substring(2, 3);
 			numbers = numbers.substring(3);
 		}
-		
+		return numbers;
+	}
+	
+	public int add(String numbers) throws RuntimeException {
+		int result = 0;
+		numbers = parserEmptyString(numbers);
+		numbers = setupDefaultSplit(numbers);
 		numbers = numbers.replaceAll("\n", split);
-		
 		valideInput(numbers);
-		
 		String[] args = numbers.split(split);
-		
+		return doCalculate(args);
+	}
+	
+	private int doCalculate(String[] args){
+		int result = 0;
 		for (String parameter : args){
-			
-			if (parameter.isEmpty()){
-				parameter = "0";
-			}
-			
+			parameter = parserEmptyString(parameter);				
 			result += Integer.parseInt(parameter);
 		}
-		
 		return result;
 	}
 
