b85fa44c351579493498047ebba48de369f6f541
Guo KaiNi <ken@webcomm.com.tw>
2017-06-26 22:54:03 +0800

    調整程式，使其通過測試案例

--- src/main/java/com/stringcalculate/kata/StringCalculator.java
+++ src/main/java/com/stringcalculate/kata/StringCalculator.java
@@ -4,7 +4,10 @@ public class StringCalculator {
 
 	public int add(String numbers) {
 		// TODO Auto-generated method stub
-		return 0;
+		if (numbers.isEmpty()){
+			return 0;
+		}
+		return Integer.parseInt(numbers);
 	}
 
 }
